c8b840e5f1df01f4382c23b4361f8ac913439e38
Faustino Correa <faustino.correa.m@gmail.com>
2022-06-24 22:43:15 -0400

    sales_by_match.py

---
+++ warm-up_challenges/sales_by_match/sales_by_match.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+
+    colors = []
+    pairs = 0
+
+    for i in range(n):
+        if ar[i] not in colors:
+            colors.append(ar[i])
+        else:
+            colors.remove(ar[i])
+            pairs += 1
+
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
